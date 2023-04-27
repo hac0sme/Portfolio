780d533768892a0ff3afd2f044d41110b56d9e8a
Hugo <hugocos11@gmail.com>
2023-04-27 14:57:02 -0500

    first commit of portfolio

---
+++ README.md
@@ -0,0 +1,2 @@
+Este es un proyecto personal enfocado a desplegar mi portafolio de trabajos y desarrollos software que he realizado.
+Autor: Hugo Cosme
---
+++ portfolio/__init__.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, abort
+
+app = Flask(__name__)
+projects = [
+    {
+        "name": "Habit tracking app with Python and MongoDB",
+        "thumb": "img/habit-tracking.png",
+        "hero": "img/habit-tracking-hero.png",
+        "categories": ["python", "web"],
+        "slug": "habit-tracking",
+        "prod": "https://udemy.com",
+    },
+    {
+        "name": "Personal finance tracking app with React",
+        "thumb": "img/personal-finance.png",
+        "hero": "img/personal-finance.png",
+        "categories": ["react", "javascript"],
+        "slug": "personal-finance",
+    },
+    {
+        "name": "REST API Documentation with Postman and Swagger",
+        "thumb": "img/rest-api-docs.png",
+        "hero": "img/rest-api-docs.png",
+        "categories": ["writing"],
+        "slug": "api-docs",
+    },
+]
+
+slug_to_project = {project["slug"]: project for project in projects}
+
+@app.route("/")
+def home():
+    return render_template("home.html.j2", projects=projects)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html.j2")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html.j2")
+
+@app.route("/project/<string:slug>")
+def project(slug):
+    if slug not in slug_to_project:
+        abort(404)
+    return render_template(f"project_{slug}.html.j2", project=slug_to_project[slug])
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html.j2"), 404
