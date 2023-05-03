a092e583d4c4d2be76fd7dad4fb2bc7c877c19fb
Hugo <hugocos11@gmail.com>
2023-05-03 02:43:41 -0500

    Renaming all templates

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -42,25 +42,25 @@ slug_to_project = {project["slug"]: project for project in projects}
 
 @app.route("/")
 def home():
-    return render_template("home.html.j2", projects=projects)
+    return render_template("home.html", projects=projects)
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html.j2")
+    return render_template("about.html")
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html.j2")
+    return render_template("contact.html")
 
 @app.route("/project/<string:slug>")
 def project(slug):
     if slug not in slug_to_project:
         abort(404)
-    return render_template(f"project_{slug}.html.j2", project=slug_to_project[slug])
+    return render_template(f"project_{slug}.html", project=slug_to_project[slug])
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template("404.html.j2"), 404
+    return render_template("404.html"), 404
